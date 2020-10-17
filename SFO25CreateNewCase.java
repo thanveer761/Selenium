6d6be916dd0b72f7c1ec8fc0e677e055dbfb1adb
Thanveer Ahmed <70330203+thanveer761@users.noreply.github.com>
2020-10-17 18:15:43 +0530

    Add files via upload
    firefox

--- SFO25CreateNewCase.java
+++ SFO25CreateNewCase.java
@@ -1,69 +1,81 @@
 package week1.day1;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.GeckoDriverInfo;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SFO25CreateNewCase {
+	
+public static WebDriver driver;
 
 	public static void main(String[] args) throws InterruptedException {
-		//disable Chrome notifications
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--disable-notifications");
-		
-		//get driver
-		WebDriverManager.chromedriver().setup();
-		ChromeDriver driver = new ChromeDriver(options);
+		FirefoxOptions options_firfox = new FirefoxOptions();
+		options_firfox.setProfile(new FirefoxProfile());
+		options_firfox.addPreference("dom.webnotifications.enabled", false);
+        WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver(options_firfox);
 		
 		//launch Url
 	driver.get("https://login.salesforce.com");
-	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	//driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	driver.manage().window().maximize();
+	WebDriverWait wait = new WebDriverWait(driver, 20);
 	
 	//click login
-	driver.findElementById("username").sendKeys("nupela@testleaf.com");
-	driver.findElementById("password").sendKeys("Bootcamp$123");
-	driver.findElementById("Login").click();
+	driver.findElement(By.id("username")).sendKeys("nupela@testleaf.com");
+	driver.findElement(By.id("password")).sendKeys("Bootcamp$123");
+	driver.findElement(By.id("Login")).click();
 	
 	//click global actions svg icon
 
 	Thread.sleep(30000);
-	
-	WebElement element = driver.findElementByXPath("//*[@data-key='add']");
+	WebElement element = driver.findElement(By.xpath("//*[@data-key='add']"));
+	//WebElement element = driver.findElementByXPath("//*[@data-key='add']");
 	element.click();
 	
 	//click newCase
 	//Thread.sleep(10000);
-	 driver.findElementByXPath("//span[text()='New Case']").click();
+	driver.findElement(By.xpath("//span[text()='New Case']")).click();
 
    //click contact name
 	Thread.sleep(10000);
-	driver.findElementByXPath("//input[@title='Search Contacts']").click();
-	 driver.findElementByXPath("//div[text()='Naveen Elumalai']").click();
+	// driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	driver.findElement(By.xpath("//input[@title='Search Contacts']")).click();
+	Thread.sleep(10000);
+	driver.findElement(By.xpath("//div[text()='Naveen Elumalai']")).click();
 	 
 
 //choose dropDown status as escalated
-	 driver.findElementByXPath("//*[text()='New']").click();
-driver.findElementByXPath("//a[@title='Escalated']").click();
+	 driver.findElement(By.xpath("//*[text()='New']")).click();
+driver.findElement(By.xpath("//a[@title='Escalated']")).click();
 
 //enter subject  as testing
-driver.findElementByXPath("//span[text()='Subject']/following::input").sendKeys("testing");
+driver.findElement(By.xpath("//span[text()='Subject']/following::input")).sendKeys("testing");
 
 //enter description as dummy
-driver.findElementByXPath("//span[text()='Description']/following::textarea").sendKeys("dummy");
+driver.findElement(By.xpath("//span[text()='Description']/following::textarea")).sendKeys("dummy");
 
 //click save
-driver.findElementByXPath("(//span[text()='Save'])[2]").click();
+driver.findElement(By.xpath("(//span[text()='Save'])[2]")).click();
 
-System.out.println("new case created"
-
+System.out.println("new case created");
 	}
 
 }
